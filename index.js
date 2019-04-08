741cae1bfcd17fcc1c116f011282949fe1b843c2
Didac Bigorda <didacbigorda@gmail.com>
2019-04-08 23:50:26 +0200

    Add actions

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+{
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name 'Learn Redux',
+    complete: false,
+  }
+}
+
+{
+  type: 'REMOVE_TODO',
+  id: 0,
+}
+
+{
+  type: 'TOGGLE_TODO',
+  id: 0,
+}
+
+{
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Climb Everest',
+  }
+}
+
 function createStore () {
   // the store should have four parts
   // 1. The state
