d9508dffe3e972183759a9672e0b5b7cd23aa482
Didac Bigorda <didacbigorda@gmail.com>
2019-04-09 17:06:51 +0200

    Add variables for actions

--- index.js
+++ index.js
@@ -1,45 +1,4 @@
-/*
-  Characteristics of a Pure function
-  1) They always return the same result if the same argumetns are passed in
-  2) They depend only on the arguments passed into them
-  3) Never produce any side effects
-*/
-
-
-// Reducer function
-function todos (state = [], action) {
-  switch(action.type) {
-    case 'ADD_TODO':
-      return state.concat([action.todo])
-    case 'REMOVE_TODO':
-      return state.filter((todo) => todo.id !== action.id )
-    case 'TOGGLE_TODO':
-      return state.map((todo) => todo.id !== action.id ? todo : 
-        Object.assign({}, todo, {complete: !todo.complete})
-      )
-    default:
-      return state
-  }
-}
-
-
-function goals (state = [], action) {
-  switch(action.type) {
-    case 'ADD_GOAL':
-    return state.concat([action.goal])
-    case 'REMOVE_GOAL':
-    return state.filter((goal) => goal.id !== action.id)
-    default:
-    return state
-  }
-}
-
-function app (state = {}, action) {
-  return {
-    todos: todos(state.todos, action),
-    goals: goals(state.goals, action)
-  }
-}
+// Library Code
 
 function createStore (reducer) {
   // the store should have four parts
@@ -70,6 +29,49 @@ function createStore (reducer) {
     getState,
     subscribe,
     dispatch,
+  }
+}
+
+// App Code
+
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
+// Reducer function
+function todos (state = [], action) {
+  switch(action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo])
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id )
+    case TOGGLE_TODO:
+      return state.map((todo) => todo.id !== action.id ? todo : 
+        Object.assign({}, todo, {complete: !todo.complete})
+      )
+    default:
+      return state
+  }
+}
+
+
+function goals (state = [], action) {
+  switch(action.type) {
+    case ADD_GOAL:
+    return state.concat([action.goal])
+    case REMOVE_GOAL:
+    return state.filter((goal) => goal.id !== action.id)
+    default:
+    return state
+  }
+}
+
+function app (state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
   }
 }
 
