5583719c56a9d84db4f9c2830dfc6c76acc7d8c1
Didac Bigorda <didacbigorda@gmail.com>
2019-04-09 16:07:17 +0200

    Add more actions to reducer function

--- index.js
+++ index.js
@@ -35,11 +35,18 @@
 
 // Reducer function
 function todos (state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
+  switch(action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.todo])
+    case 'REMOVE_TODO':
+      return state.filter((todo) => todo.id !== action.id )
+    case 'TOGGLE_TODO':
+      return state.map((todo) => todo.id !== action.id ? todo : 
+        Object.assign({}, todo, {complete: !todo.complete})
+      )
+    default:
+      return state
   }
-
-  return state
 }
 
 function createStore (reducer) {
@@ -75,11 +82,6 @@ function createStore (reducer) {
 }
 
 const store = createStore(todos);
-store.dispatch({
-  type: 'ADD_TODO',
-  todo: {
-    id: 0,
-    name:'Learn Redux',
-    complete: false,
-  }
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
 })
